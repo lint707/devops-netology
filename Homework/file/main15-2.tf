f49c5491a527e3b86158ce7718869292cab8a09b
Grigorii Kazachenko <100125670+lint707@users.noreply.github.com>
2023-05-09 15:31:35 +0300

    Add files via upload

---
+++ Homework/file/main15-2.tf
@@ -0,0 +1,164 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.90.0"
+    }
+  }
+  required_version = ">= 0.13"
+}
+variable "yc_token" {
+  type = string
+  description = "Yandex Cloud API key"
+}
+variable "yc_region" {
+  type = string
+  description = "Yandex Cloud Region (i.e. ru-central1-a)"
+}
+variable "yc_cloud_id" {
+  type = string
+  description = "Yandex Cloud id"
+}
+variable "yc_folder_id" {
+  type = string
+  description = "Yandex Cloud folder id"
+}
+
+provider "yandex" {
+  token = var.yc_token
+  cloud_id  = var.yc_cloud_id
+  folder_id = "b1g0qaa93f2na9813k1q"
+  zone      = var.yc_region
+}
+
+resource "yandex_iam_service_account" "sa" {
+  name = "netl"
+}
+
+// Назначение роли сервисному аккаунту
+resource "yandex_resourcemanager_folder_iam_member" "sa-editor" {
+  folder_id = "b1g0qaa93f2na9813k1q"
+  role      = "storage.editor"
+  member    = "serviceAccount:${yandex_iam_service_account.sa.id}"
+}
+
+// Создание статического ключа доступа
+resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+  service_account_id = yandex_iam_service_account.sa.id
+  description        = "static access key for object storage"
+}
+
+// Создание бакета с использованием ключа
+resource "yandex_storage_bucket" "vasya" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "vasya-20-11-2000"
+  anonymous_access_flags {
+    read = true
+    list = false
+  }
+}
+resource "yandex_storage_object" "object" {
+  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  bucket     = "vasya-20-11-2000"
+  key        = "elk.jpg"
+  source     = "/home/user/bkt/elk.jpg"
+
+}
+resource "yandex_iam_service_account" "igsa" {
+  name        = "igsa"
+  description = "service account to manage IG"
+}
+
+resource "yandex_resourcemanager_folder_iam_member" "editor" {
+  folder_id = "b1g0qaa93f2na9813k1q"
+  role      = "editor"
+  member   = "serviceAccount:${yandex_iam_service_account.igsa.id}"
+}
+
+resource "yandex_compute_instance_group" "ig-1" {
+  name               = "fixed-ig-with-balancer"
+  folder_id          = "b1g0qaa93f2na9813k1q"
+  service_account_id = "${yandex_iam_service_account.igsa.id}"
+  instance_template {
+    platform_id = "standard-v3"
+    resources {
+      memory = 2
+      cores  = 2
+    }
+
+    boot_disk {
+      mode = "READ_WRITE"
+      initialize_params {
+        image_id = "fd827b91d99psvq5fjit"
+      }
+    }
+
+    network_interface {
+      network_id = yandex_vpc_network.network.id
+      subnet_ids = ["${yandex_vpc_subnet.subnet-1.id}"]
+      nat = true
+    }
+
+    metadata = {
+      user_data = templatefile("${path.module}/user_data.sh", { image_url = "https://storage.yandexcloud.net/vasya-20-11-2000/elk.jpg" })
+    }
+  }
+
+  scale_policy {
+    fixed_scale {
+      size = 3
+    }
+  }
+
+  allocation_policy {
+    zones = ["ru-central1-a"]
+  }
+
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
+
+  load_balancer {
+    target_group_name        = "target-group"
+    target_group_description = "load balancer target group"
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "ntlb-1" {
+  name = "network-load-balancer-1"
+
+  listener {
+    name = "network-load-balancer-1-listener"
+    port = 80
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = yandex_compute_instance_group.ig-1.load_balancer.0.target_group_id
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 80
+        path = "/index.html"
+      }
+    }
+  }
+}
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  name           = "subnet1"
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = ["192.168.10.0/24"] 
+}
+
+
